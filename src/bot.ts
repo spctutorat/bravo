3a7155de22f68ae9a7cc65ae35c04268ef75c11d
Anatole Debierre <62328077+a2br@users.noreply.github.com>
2021-09-30 15:16:51 +0200

    Init Roles function

--- src/bot.ts
+++ src/bot.ts
@@ -2,12 +2,17 @@ import Discord, { Intents } from "discord.js";
 import { pingHandler } from "./commands/ping";
 import { env } from "./util/env";
 import { registerInteractions } from "./util/registerInteractions";
+import { initRoles } from "./util/roles";
 
 const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // Discord bot
 client.on("ready", async () => {
 	await registerInteractions();
+	const guild =
+		client.guilds.cache.get(env("GUILD_ID")) ||
+		(await client.guilds.fetch(env("GUILD_ID")));
+	await initRoles(guild);
 	console.log(`Logged in as ${client.user?.tag}!`);
 });
 
--- src/util/roles.ts
+++ src/util/roles.ts
@@ -1,5 +1,7 @@
 // List all roles
 
+import { Guild } from "discord.js";
+
 export type Niveau = "6" | "5" | "4" | "3" | "2" | "1" | "T";
 
 export const rolesNiveau: Record<Niveau, string> = {
@@ -11,3 +13,15 @@ export const rolesNiveau: Record<Niveau, string> = {
 	"1": "Première",
 	T: "Terminale",
 };
+
+export async function initRoles(guild: Guild): Promise<void> {
+	const roleNames = ["OK", ...Object.values(rolesNiveau)];
+	await guild.roles.fetch();
+	await Promise.all(
+		roleNames.map(async n => {
+			const existing = guild.roles.cache.find(f => f.name === n);
+			if (!existing)
+				await guild.roles.create({ name: n, reason: "Initiating roles" });
+		})
+	);
+}
