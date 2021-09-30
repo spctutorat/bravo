47136aa5fdc787c7699efdf6d64d6eac3235a38d
Anatole Debierre <62328077+a2br@users.noreply.github.com>
2021-09-30 21:19:55 +0200

    Add logout

--- src/bot.ts
+++ src/bot.ts
@@ -1,13 +1,21 @@
 import Discord, { Intents } from "discord.js";
 import { idHandler } from "./commands/id";
 import { loginHandler } from "./commands/login";
+import { logoutHandler } from "./commands/logout";
 import { pingHandler } from "./commands/ping";
 import { rolesHandler } from "./commands/roles";
 import { env } from "./util/env";
 import { registerInteractions } from "./util/registerInteractions";
 import { initRoles } from "./util/roles";
 
-const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Discord.Client({
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+	],
+	partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
 
 // Discord bot
 client.on("ready", async () => {
@@ -31,6 +39,11 @@ client.on("interactionCreate", async interaction => {
 
 	if (interaction.commandName === "login") {
 		loginHandler(client, interaction);
+		return;
+	}
+
+	if (interaction.commandName === "logout") {
+		logoutHandler(client, interaction);
 		return;
 	}
 
---
+++ src/commands/logout.ts
@@ -0,0 +1,69 @@
+import prisma from "../util/db";
+import * as Discord from "discord.js";
+import { InteractionHandler } from "../util/types";
+import { rolesNiveau } from "../util/roles";
+
+export const logoutHandler: InteractionHandler = async (
+	client,
+	interaction
+) => {
+	// Check if already logged in
+	const doc = await prisma.user.findUnique({
+		where: { discordId: interaction.user.id },
+	});
+	if (doc) {
+		// Logged in
+		// Ask for confirmation
+		await interaction.deferReply();
+		const reply = (await interaction.editReply(
+			"Se déconnecter ?"
+		)) as Discord.Message;
+		const filter: Discord.CollectorFilter<
+			[Discord.MessageReaction, Discord.User]
+		> = (reaction, user) => {
+			const a =
+				(["✅", "❌"] as (string | null)[]).includes(reaction.emoji.name) &&
+				user.id === interaction.user.id;
+			console.log({ a });
+			return a;
+		};
+		reply
+			.awaitReactions({ max: 1, filter, time: 10000, errors: ["time"] })
+			.then(async collected => {
+				const reaction = collected.first();
+				if (reaction?.emoji.name === "✅") {
+					// Validate
+					await prisma.user.delete({
+						where: { discordId: interaction.user.id },
+					});
+					// Remove roles
+					const member = interaction.member as Discord.GuildMember;
+					await member.fetch();
+					await Promise.all(
+						member.roles.cache
+							.filter(r => ["OK", Object.values(rolesNiveau)].includes(r.name))
+							.map(r => member.roles.remove(r))
+					);
+
+					// Inform user
+					await interaction.editReply(
+						"_Compte supprimé. Vos données ont été supprimées._"
+					);
+				} else {
+					// Cancel
+					await interaction.editReply("_Déconnexion annulée._");
+				}
+			})
+			.catch(async collected => {
+				await interaction.editReply("_Aucune réaction : Déconnexion annulée._");
+			});
+		await reply.react("✅");
+		await reply.react("❌");
+	} else {
+		// Logged out
+		await interaction.reply({
+			content:
+				"Vous n'êtes pas identifié.e. Utilisez `/login` pour vous connecter.",
+		});
+	}
+};
